b1daa4dac50ed93c4b9ec082a5ee3ce6c33a4cba
Mansin <benjaminhammerthygesen@yahoo.dk>
2025-11-06 13:03:42 +0100

    added player example

--- audio-streamer/app/components/player.tsx
+++ audio-streamer/app/components/player.tsx
@@ -1,51 +1,67 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { io, Socket } from "socket.io-client";
 import { AudioStreamPlayer } from "../audio/audio-stream-player";
 
-let socket: Socket;
-let audioStreamPlayer: AudioStreamPlayer;
-let isConnected = false;
-let stateInterval: NodeJS.Timeout;
-
-async function connect() {
-  if (!isConnected) {
-    console.log("Joining audio!");
-    socket = io(":8080");
-
-    socket.on("newSet", () => {
-      if (isConnected) {
-        console.log("hallo!");
-        audioStreamPlayer.reset();
-        audioStreamPlayer.start();
-      }
-    });
-
-    audioStreamPlayer = new AudioStreamPlayer(socket, 10, "OPUS");
-
-    audioStreamPlayer.reset();
-    audioStreamPlayer.start();
-    isConnected = true;
-  }
-}
-
-async function disconnect() {
-  if (isConnected) {
-    clearInterval(stateInterval);
-    audioStreamPlayer.close();
-    audioStreamPlayer = null;
-    socket.disconnect();
-    isConnected = false;
-  }
-}
 
 export function Player() {
-  useEffect(function mount() {});
+  const [audioStreamPlayer, setAudioStreamPlayer] = useState<AudioStreamPlayer>(null)
+  const [isConnected, setIsConnected] = useState(false)
+  const [stateInterval, setStateInterval] = useState<NodeJS.Timeout>(null)
+  const [playState, setPlayState] = useState([0, 0])
+  let socket: Socket;
+  
+  async function connect() {
+    if (!isConnected) {
+      console.log("Joining audio!");
+
+      socket.connect()
+      socket.on("newSet", () => {
+        if (isConnected) {
+          console.log("hallo!");
+          audioStreamPlayer.reset();
+          audioStreamPlayer.start();
+        }
+      });
+  
+      const player = new AudioStreamPlayer(socket, 10, "OPUS")
+      player.reset();
+      player.start();
+      setAudioStreamPlayer(player);
+
+      setStateInterval(setInterval(() => {
+        setPlayState([player.getCurrentPlayPosition(), player.getTotalDuration()])
+      }, 1000))
+  
+      setIsConnected(true);
+    }
+  }
+  
+  async function disconnect() {
+    if (isConnected) {
+      clearInterval(stateInterval);
+      setStateInterval(null)
+
+      audioStreamPlayer.close();
+      setAudioStreamPlayer(null);
+      
+      socket.disconnect();
+      
+      setIsConnected(false)
+    }
+  }
+
+  useEffect(function mount() {
+    socket = io(":8080", {autoConnect: false})
+  });
 
   return (
     <div>
       Playing
       <button onClick={connect}>connect</button>
       <button onClick={disconnect}>disconnect</button>
+      <div>
+        Playing: {playState[0]} / {playState[1]}
+      </div>
     </div>
   );
 }
