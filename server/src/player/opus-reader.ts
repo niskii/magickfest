ac8f0690423ad4b8c47e1c3a39f9b3fb987320d5
Mansin <benjaminhammerthygesen@yahoo.dk>
2025-11-27 01:15:40 +0100

    opus reader docs

--- server/src/player/opus-reader.ts
+++ server/src/player/opus-reader.ts
@@ -35,34 +35,74 @@ export class OpusReader {
     this.#positions = this.#pages.flatMap((p) => Number(p.position));
   }
 
-  startClock() {
+  /**
+   * Resets the internal clock of the reader.
+   */
+  resetClock() {
     this.#startTime = Date.now();
   }
 
+  /**
+   * Get the internal starting point of the reader.
+   *
+   * @returns unix time point
+   */
   getClock() {
     return this.#startTime;
   }
 
+  /**
+   * Set the internal clock of the reader.
+   *
+   * @param time unix time
+   */
   setClock(time: number) {
     this.#startTime = time;
   }
 
+  /**
+   * Returns the current play position in milliseconds.
+   *
+   * @returns play position in milliseconds
+   */
   getCurrentTimeMillis() {
     return Date.now() - this.#startTime;
   }
 
+  /**
+   * Returns the remaining time of the file given the position.
+   *
+   * @returns reamining time in seconds
+   */
   getRemainingTimeSeconds() {
     return this.#totalDurationSeconds - this.getCurrentTimeMillis() / 1000;
   }
 
+  /**
+   * Returns the current play position in seconds.
+   *
+   * @returns play position in seconds
+   */
   getPlayTimeSeconds() {
     return (Date.now() - this.#startTime) / 1000;
   }
 
+  /**
+   * Returns the page number of the current play position.
+   *
+   * @returns page number
+   */
   getCurrentPage() {
-    return this.bytesToPage(this.getPlayTimeSeconds() * 48000);
+    return this.searchPosition(this.getPlayTimeSeconds() * 48000);
   }
 
+  /**
+   * Returns the duration of a range of pages.
+   *
+   * @param pageStart start page number
+   * @param pageEnd end page number
+   * @returns number in seconds
+   */
   calculateRangeDuration(pageStart: number, pageEnd: number) {
     if (
       pageStart < 0 ||
@@ -78,10 +118,24 @@ export class OpusReader {
     );
   }
 
+  /**
+   * Determines how many pages are needed to satisfy the duration.
+   *
+   * @param minDuration minimal duration allowed
+   * @param pageDuration duration of a single page
+   * @returns number of pages need
+   */
   calculateMinPages(minDuration: number, pageDuration: number) {
     return Math.max(Math.floor(minDuration / pageDuration), 1);
   }
 
+  /**
+   * Returns index of target element
+   *
+   * @param arr array to search
+   * @param target target number
+   * @returns index in array
+   */
   #binarySearch(arr: number[], target: number): number {
     let low = 0;
     let high = arr.length - 1;
@@ -100,14 +154,33 @@ export class OpusReader {
     return low;
   }
 
-  bytesToPage(position: number) {
+  /**
+   * Returns the nearest page to the position.
+   *
+   * @param position target position for the search
+   * @returns the found page number
+   */
+  searchPosition(position: number) {
     return this.#binarySearch(this.#positions, position);
   }
 
+  /**
+   * Returns the page number with a predetermined distance to the parameter.
+   *
+   * @param pageStart number to start the range
+   * @returns number of the page ending the range
+   */
   getPageRangeEnd(pageStart: number) {
     return Math.min(pageStart + this.#minPagesForChunk, this.#numberOfPages);
   }
 
+  /**
+   * Returns an object containing a slice of audio with metadata.
+   *
+   * @param start page number to start the slice
+   * @param end page number to end the slice
+   * @returns AudioPacket containing audio buffer slice
+   */
   makeChunkFromRange(start: number, end: number) {
     const chunks = this.#fileSplitter.sliceByPage(start, end);
     if (chunks !== null) {
@@ -125,6 +198,11 @@ export class OpusReader {
     }
   }
 
+  /**
+   * Returns the AudioPacket at the current play position.
+   *
+   * @returns an AudioPacket
+   */
   getCurrentChunk() {
     const pageStart = this.getCurrentPage();
     const pageEnd = this.getPageRangeEnd(pageStart);
@@ -135,17 +213,17 @@ export class OpusReader {
     };
   }
 
+  /**
+   * Returns an AudioPacket of default duration.
+   *
+   * @param pageStart starting point for the range
+   * @returns an AudioPacket
+   */
   getNextChunk(pageStart: number) {
     const currentPage = this.getCurrentPage();
     const pageEnd = this.getPageRangeEnd(pageStart);
     if (pageStart == pageEnd) return { data: null, status: ReadCode.EOF };
 
-    console.log(
-      "more data requested! %d, %d, %d",
-      currentPage,
-      pageStart,
-      this.#numberOfPages - 1,
-    );
     if (pageStart > this.#numberOfPages || pageEnd > this.#numberOfPages)
       return { data: null, status: ReadCode.EOF };
     if (
