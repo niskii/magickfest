97aec850ad9265c2a074186c7d7771e8d44105ff
Mansin <benjaminhammerthygesen@yahoo.dk>
2025-12-03 16:50:15 +0100

    server rate limit

--- server/src/api/middlewares.ts
+++ server/src/api/middlewares.ts
@@ -2,6 +2,7 @@ import bodyParser from "body-parser";
 import connectSqlite3 from "connect-sqlite3";
 import cors from "cors";
 import { Express, NextFunction, Request, Response } from "express";
+import rateLimit from "express-rate-limit";
 import session, { Store } from "express-session";
 import helmet from "helmet";
 import { Server } from "socket.io";
@@ -9,11 +10,40 @@ import { UserManager } from "src/user/user-manager";
 import authAPI, { isAuthorized } from "../api/auth";
 import serviceAPI from "../api/service";
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 15,
+  legacyHeaders: false,
+});
+
+const SQLiteStore = connectSqlite3(session);
+
+const sessionMiddleware = session({
+  secret: process.env.SessionSecret!,
+  resave: false,
+  name: "s.id",
+
+  saveUninitialized: false,
+  store: new SQLiteStore({
+    table: "sessions",
+    db: "sessions.db",
+    dir: "./temp",
+  }) as Store,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 10,
+    sameSite: "lax",
+    secure: true,
+    httpOnly: true,
+  },
+});
+
 export function setupMiddleware(
   app: Express,
   io: Server,
   userManager: UserManager,
 ) {
+  app.use(limiter);
+  app.disable("x-powered-by");
   app.use(
     cors({
       origin: globalThis.settings.origins,
@@ -21,29 +51,6 @@ export function setupMiddleware(
       allowedHeaders: ["Access-Control-Allow-Origin"],
     }),
   );
-
-  const SQLiteStore = connectSqlite3(session);
-
-  const sessionMiddleware = session({
-    secret: process.env.SessionSecret!,
-    resave: false,
-    name: "s.id",
-
-    saveUninitialized: false,
-    store: new SQLiteStore({
-      table: "sessions",
-      db: "sessions.db",
-      dir: "./temp",
-    }) as Store,
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24 * 10,
-      sameSite: "lax",
-      secure: true,
-      httpOnly: true,
-    },
-  });
-
-  app.disable("x-powered-by");
   app.use(helmet());
   app.use(bodyParser.json());
   app.use(sessionMiddleware);
