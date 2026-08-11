a35452b251e21fc40b4baa6375e9127daa190bd6
Mansin <benjaminhammerthygesen@yahoo.dk>
2026-08-11 16:00:17 +0200

    feat: ​Encourage Hardware Acceleration

--- frontend/src/bootstrap.ts
+++ frontend/src/bootstrap.ts
@@ -2,130 +2,123 @@ import { reactive } from "vue";
 import logger from "./logger";
 
 export const bootstrap = reactive({
-  status: "loading" as "loading" | "ready" | "error",
-  step: "starting...",
-  error: null as string,
-  auth: null as any,
-  layout: 0 as number,
-  platform: null as string
+    status: "loading" as "loading" | "ready" | "error",
+    step: "starting...",
+    error: null as string,
+    auth: null as any,
+    layout: 0 as number,
+    platform: null as string,
 });
 
 export async function bootstrapDiscord() {
-  const clientID = import.meta.env.VITE_DISCORD_CLIENT_ID;
-  const isOnDiscord = window.location.hostname.includes(clientID);
-  let auth;
+    const clientID = import.meta.env.VITE_DISCORD_CLIENT_ID;
+    const isOnDiscord = window.location.hostname.includes(clientID);
+    let auth;
 
-  if (!isOnDiscord) {
+    if (!isOnDiscord) {
+        bootstrap.status = "ready";
+        bootstrap.auth = null;
+        return;
+    }
+
+    bootstrap.step = "loading Discord SDK...";
+    const DiscordSDK = await import("@discord/embedded-app-sdk").catch((err) => {
+        throw new Error(`could not load the SDK dynamically: ${err}`);
+    });
+
+    // Instantiate the SDK
+    bootstrap.step = "connecting to Discord...";
+    const discordSdk = new DiscordSDK.DiscordSDK(clientID);
+
+    await setupDiscordSdk();
+    logger.info("Discord SDK is ready");
+
     bootstrap.status = "ready";
-    bootstrap.auth = null;
-    return;
-  }
+    bootstrap.auth = auth;
 
-  bootstrap.step = "loading Discord SDK...";
-  const DiscordSDK = await import("@discord/embedded-app-sdk").catch((err) => {
-    throw new Error(`could not load the SDK dynamically: ${err}`);
-  });
-  
-  // Instantiate the SDK
-  bootstrap.step = "connecting to Discord...";
-  const discordSdk = new DiscordSDK.DiscordSDK(clientID);
+    async function setupDiscordSdk() {
+        bootstrap.step = "setting up Discord SDK...";
+        await discordSdk.ready().catch((err: Error) => {
+            throw new Error(`the sdk could not setup: ${err}`);
+        });
 
-  await setupDiscordSdk();
-  logger.info("Discord SDK is ready");
+        bootstrap.step = "Encouraging Hardware Acceleration";
+        await discordSdk.commands.encourageHardwareAcceleration();
 
-  bootstrap.status = "ready";
-  bootstrap.auth = auth;
+        // Authorize with Discord Client
+        bootstrap.step = "authorizing...";
+        const { code } = await discordSdk.commands
+            .authorize({
+                client_id: clientID,
+                response_type: "code",
+                state: "",
+                prompt: "none",
+                scope: ["identify", "guilds", "guilds.members.read"],
+            })
+            .catch((err: Error) => {
+                throw new Error(`could not authorize at Discord's end: ${err}`);
+            });
 
-  async function setupDiscordSdk() {
-    bootstrap.step = "setting up Discord SDK...";
-    await discordSdk.ready().catch((err: Error) => {
-      throw new Error(`the sdk could not setup: ${err}`);
-    });
+        // Retrieve an access_token from your activity's server
+        bootstrap.step = "retrieving access token...";
+        let response = await fetch("/api/auth/token", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                code,
+            }),
+        }).catch((err) => {
+            bootstrap.status = "error";
+            throw new Error(`the backend server could not authenticate the user with the provided code: ${err}`);
+        });
 
-    // Authorize with Discord Client
-    bootstrap.step = "authorizing...";
-    const { code } = await discordSdk.commands
-      .authorize({
-        client_id: clientID,
-        response_type: "code",
-        state: "",
-        prompt: "none",
-        scope: ["identify", "guilds", "guilds.members.read"],
-      })
-      .catch((err: Error) => {
-        throw new Error(`could not authorize at Discord's end: ${err}`);
-      });
+        if (response && !response.ok) {
+            const text = await response.text();
+            throw new Error(`the backend server could not authenticate the user with the provided code: ${text}`);
+        }
 
-    // Retrieve an access_token from your activity's server
-    bootstrap.step = "retrieving access token...";
-    let response = await fetch("/api/auth/token", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        code,
-      }),
-    }).catch((err) => {
-      bootstrap.status = "error";
-      throw new Error(
-        `the backend server could not authenticate the user with the provided code: ${err}`,
-      );
-    });
+        const { access_token } = await response.json().catch((err: Error) => {
+            throw new Error(`could not parse the token provided from the server: ${err}`);
+        });
 
-    if (response && !response.ok) {
-      const text = await response.text();
-      throw new Error(
-        `the backend server could not authenticate the user with the provided code: ${text}`,
-      );
+        // Authenticate with Discord client (using the access_token)
+        bootstrap.step = "authenticating...";
+        auth = await discordSdk.commands
+            .authenticate({
+                access_token,
+            })
+            .catch((err: Error) => {
+                throw new Error(`the token provided from the server isn't accepted by Discord: ${err}`);
+            });
+
+        if (auth == null) {
+            throw new Error(`Authenticate command failed`);
+        }
+
+        bootstrap.step = "starting session...";
+        response = await fetch("/api/auth/startsession", {
+            method: "POST",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                access_token,
+            }),
+        }).catch((err) => {
+            throw new Error(err);
+        });
+
+        if (response && !response.ok) {
+            const text = await response.text();
+            throw new Error(`Not member of group, discord error or couldn't save cookie: ${text}`);
+        }
+
+        bootstrap.platform = discordSdk.platform;
+        discordSdk.subscribe("ACTIVITY_LAYOUT_MODE_UPDATE", (event) => {
+            bootstrap.layout = event.layout_mode;
+        });
     }
-
-    const { access_token } = await response.json().catch((err: Error) => {
-      throw new Error(
-        `could not parse the token provided from the server: ${err}`,
-      );
-    });
-
-    // Authenticate with Discord client (using the access_token)
-    bootstrap.step = "authenticating...";
-    auth = await discordSdk.commands
-      .authenticate({
-        access_token,
-      })
-      .catch((err: Error) => {
-        throw new Error(
-          `the token provided from the server isn't accepted by Discord: ${err}`,
-        );
-      });
-
-    if (auth == null) {
-      throw new Error(`Authenticate command failed`);
-    }
-
-    bootstrap.step = "starting session...";
-    response = await fetch("/api/auth/startsession", {
-      method: "POST",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        access_token,
-      }),
-    }).catch((err) => {
-      throw new Error(err);
-    });
-
-    if (response && !response.ok) {
-      const text = await response.text();
-      throw new Error(
-        `Not member of group, discord error or couldn't save cookie: ${text}`,
-      );
-    }
-
-    bootstrap.platform = discordSdk.platform
-    discordSdk.subscribe("ACTIVITY_LAYOUT_MODE_UPDATE", (event) => {
-      bootstrap.layout = event.layout_mode
-    })
-  }
 }
